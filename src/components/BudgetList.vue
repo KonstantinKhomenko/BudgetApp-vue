dea04145b8750d602e795c9cfb129703f00e0ac7
Konstantin Khomenko <kostya5599@gmail.com>
2020-04-10 11:31:38 +0300

    budget list item

--- src/components/BudgetList.vue
+++ src/components/BudgetList.vue
@@ -2,10 +2,13 @@
   <div class="budget-list-wrap">
     <el-card :header="header">
       <template v-if="!isEmpty">
-        <div class="list-item" v-for="(item, prop) in list" :key="prop">
-          <span class="budget-comment">{{ item.comment }}</span>
-          <span class="budget-value">{{ item.value }}</span>
-          <el-button type="danger" size="mini" @click="deleteItem(item.id)">Delete</el-button>
+        <div v-for="(item, prop) in list" :key="prop">
+          <BudgetListItem
+          :comment="item.comment"
+          :value="item.value"
+          :id="Number(item.id)"
+          @deleteItemTemplate="deleteItem"
+          />
         </div>
       </template>
       <el-alert v-else type="info" :title="emptyTitle" :closable="false"/>
@@ -14,8 +17,13 @@
 </template>
 
 <script>
+import BudgetListItem from "./BudgetListItem";
+
 export default {
   name: "BudgetList",
+  components: {
+    BudgetListItem
+  },
   props: {
     list: {
       type: Object,
@@ -43,17 +51,5 @@ export default {
 .budget-list-wrap {
   max-width: 500px;
   margin: auto;
-}
-
-.list-item {
-  display: flex;
-  align-items: center;
-  padding: 10px 15px;
-}
-
-.budget-value {
-  font-weight: bold;
-  margin-left: auto;
-  margin-right: 20px;
 }
 </style>
---
+++ src/components/BudgetListItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="list-item" >
+    <span class="budget-comment">{{ comment }}</span>
+    <span class="budget-value">{{ value }}</span>
+    <el-button type="danger" size="mini" @click="deleteItemTemplate(id)">Delete</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BudgetListItem",
+  props: {
+    comment: {
+      type: String,
+      default: 'No comment'
+    },
+    value: {
+      type: Number,
+      default: 0
+    },
+    id: {
+      type: Number
+    }
+  },
+  methods: {
+    deleteItemTemplate(id) {
+      this.$emit("deleteItemTemplate", id);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.list-item {
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+}
+
+.budget-value {
+  font-weight: bold;
+  margin-left: auto;
+  margin-right: 20px;
+}
+</style>
